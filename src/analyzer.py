a191cb77d13b0cf8433d3bb9c06da0a07bdc9257
nonchris <git@chris-ge.de>
2020-11-20 21:47:05 +0100

    :arrow_upper_right: No data will now leave a gap in plot
    Added conversion from "date-string" to date-object.
    Matplot lib does this plotting now auomatically correct

--- src/analyzer.py
+++ src/analyzer.py
@@ -75,6 +75,8 @@ class Analyzer:
 
 
     def calc_data(self):
+        #convertig date-sting
+        self.df["date"] = pd.to_datetime(self.df["date"], format="%Y-%m-%d")
         self.df = self.df.set_index(["date", "location"])
         self.df = self.df.astype(int)
         #pd.to_numeric()
@@ -113,9 +115,10 @@ class Analyzer:
         #ax.plot(x_data, y_data, "rx", label="infected")
 
         #aestetics
-        plt.xticks(ticks=x_data, labels=x_data, rotation=70)
+        plt.xticks(ticks=x_data, rotation=70)
         plt.title(f"Neuinfektionen {city} - Stand {self.date}")
         plt.gcf().subplots_adjust(bottom=0.28)
+        #plt.gcf().autofmt_xdate()
         
         plt.figtext(0.5, 0.02, \
 "Tage ohne Aktualisierung der Daten werden ausgelassen.\n \
