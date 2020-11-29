12e8bb8bc2c06f00660d24eb41d5e3f752bc3e54
nonchris <git@chris-ge.de>
2020-11-29 23:47:15 +0100

    :arrow_upper_right: Updated text on other commands
    The subscription commands contain a new hint to the menu and the bot will reply with the menu when it just recieves a message without any command.

--- src/toggle_subs.py
+++ src/toggle_subs.py
@@ -10,11 +10,13 @@ def tgl_kreis(update, context):
     s = chat.settings
     if s["kreis"]:
         s["kreis"] = False
-        context.bot.send_message(text="Sie haben sich aus Updates für den Kreis Ahrweiler ausgetragen",
+        context.bot.send_message(text="Sie haben sich aus Updates für den Kreis Ahrweiler ausgetragen. \n\
+            /abo   /zeig   /menu",
                 chat_id=update.effective_chat.id)
     else:
         s["kreis"] = True
-        context.bot.send_message(text="Sie haben Updates für den Kreis Ahrweiler aboniert",
+        context.bot.send_message(text="Sie haben Updates für den Kreis Ahrweiler aboniert. \n\
+            /abo   /zeig   /menu",
                 chat_id=update.effective_chat.id)
     writer.write()
 
@@ -23,11 +25,13 @@ def tgl_adenau(update, context):
     s = chat.settings
     if s["adenau"]:
         s["adenau"] = False
-        context.bot.send_message(text="Sie haben sich aus Updates für Adenau ausgetragen",
+        context.bot.send_message(text="Sie haben sich aus Updates für Adenau ausgetragen. \n\
+            /abo   /zeig   /menu",
                 chat_id=update.effective_chat.id)
     else:
         s["adenau"] = True
-        context.bot.send_message(text="Sie haben Updates für Adenau aboniert",
+        context.bot.send_message(text="Sie haben Updates für Adenau aboniert. \n\
+            /abo   /zeig   /menu",
                 chat_id=update.effective_chat.id)
         writer.write()
     writer.write()
@@ -37,11 +41,13 @@ def tgl_altenahr(update, context):
     s = chat.settings
     if s["altenahr"]:
         s["altenahr"] = False
-        context.bot.send_message(text="Sie haben sich aus Updates für Altenahr ausgetragen",
+        context.bot.send_message(text="Sie haben sich aus Updates für Altenahr ausgetragen. \n\
+            /abo   /zeig   /menu",
                 chat_id=update.effective_chat.id)
     else:
         s["altenahr"] = True
-        context.bot.send_message(text="Sie haben Updates für Altenahr aboniert",
+        context.bot.send_message(text="Sie haben Updates für Altenahr aboniert. \n\
+            /abo   /zeig   /menu",
                 chat_id=update.effective_chat.id)
     writer.write()
 
@@ -50,11 +56,13 @@ def tgl_breisig(update, context):
     s = chat.settings
     if s["bad breisig"]:
         s["bad breisig"] = False
-        context.bot.send_message(text="Sie haben sich aus Updates für Bad Breisig ausgetragen",
+        context.bot.send_message(text="Sie haben sich aus Updates für Bad Breisig ausgetragen. \n\
+            /abo   /zeig   /menu",
                 chat_id=update.effective_chat.id)
     else:
         s["bad breisig"] = True
-        context.bot.send_message(text="Sie haben Updates für Bad Breisig aboniert",
+        context.bot.send_message(text="Sie haben Updates für Bad Breisig aboniert.\n\
+            /abo   /zeig   /menu",
                 chat_id=update.effective_chat.id)
     writer.write()
 
@@ -63,11 +71,13 @@ def tgl_brohltal(update, context):
     s = chat.settings
     if s["brohltal"]:
         s["brohltal"] = False
-        context.bot.send_message(text="Sie haben sich aus Updates für das Brohltal ausgetragen",
+        context.bot.send_message(text="Sie haben sich aus Updates für das Brohltal ausgetragen.\n\
+            /abo   /zeig   /menu",
                 chat_id=update.effective_chat.id)
     else:
         s["brohltal"] = True
-        context.bot.send_message(text="Sie haben Updates für das Brohltal aboniert",
+        context.bot.send_message(text="Sie haben Updates für das Brohltal aboniert.\n\
+            /abo   /zeig   /menu",
                 chat_id=update.effective_chat.id)
     writer.write()
 
@@ -76,11 +86,13 @@ def tgl_grafschaft(update, context):
     s = chat.settings
     if s["grafschaft"]:
         s["grafschaft"] = False
-        context.bot.send_message(text="Sie haben sich aus Updates für die Grafschaft ausgetragen",
+        context.bot.send_message(text="Sie haben sich aus Updates für die Grafschaft ausgetragen.\n\
+            /abo   /zeig   /menu",
                 chat_id=update.effective_chat.id)
     else:
         s["grafschaft"] = True
-        context.bot.send_message(text="Sie haben Updates für die Grafschaft aboniert",
+        context.bot.send_message(text="Sie haben Updates für die Grafschaft aboniert.\n\
+            /abo   /zeig   /menu",
                 chat_id=update.effective_chat.id)
     writer.write()
 
@@ -89,11 +101,13 @@ def tgl_neuenahr(update, context):
     s = chat.settings
     if s["bad neuenahr-ahrweiler"]:
         s["bad neuenahr-ahrweiler"] = False
-        context.bot.send_message(text="Sie haben sich aus Updates für Bad Neuenahr-Ahrweiler ausgetragen",
+        context.bot.send_message(text="Sie haben sich aus Updates für Bad Neuenahr-Ahrweiler ausgetragen.\n\
+            /abo   /zeig   /menu",
                 chat_id=update.effective_chat.id)
     else:
         s["bad neuenahr-ahrweiler"] = True
-        context.bot.send_message(text="Sie haben Updates für Bad Neuenahr-Ahrweiler aboniert",
+        context.bot.send_message(text="Sie haben Updates für Bad Neuenahr-Ahrweiler aboniert.\n\
+            /abo   /zeig   /menu",
                 chat_id=update.effective_chat.id)
     writer.write()
 
@@ -102,11 +116,13 @@ def tgl_remagen(update, context):
     s = chat.settings
     if s["remagen"]:
         s["remagen"] = False
-        context.bot.send_message(text="Sie haben sich aus Updates für Remagen ausgetragen",
+        context.bot.send_message(text="Sie haben sich aus Updates für Remagen ausgetragen.\n\
+            /abo   /zeig   /menu",
                 chat_id=update.effective_chat.id)
     else:
         s["remagen"] = True
-        context.bot.send_message(text="Sie haben Updates für Remagen aboniert",
+        context.bot.send_message(text="Sie haben Updates für Remagen aboniert.\n\
+            /abo   /zeig   /menu",
                 chat_id=update.effective_chat.id)
     writer.write()
 
@@ -120,12 +136,14 @@ def tgl_sinzig(update, context):
     if s["sinzig"]:
         s["sinzig"] = False
         print("sinzig: ", s["sinzig"])
-        context.bot.send_message(text="Sie haben sich aus Updates für Sinzig ausgetragen",
+        context.bot.send_message(text="Sie haben sich aus Updates für Sinzig ausgetragen.\n\
+            /abo   /zeig   /menu",
                 chat_id=update.effective_chat.id)
     else:
         s["sinzig"] = True
         print("sinzig: ", s["sinzig"])
-        context.bot.send_message(text="Sie haben Updates für Sinzig aboniert",
+        context.bot.send_message(text="Sie haben Updates für Sinzig aboniert.\n\
+            /abo   /zeig   /menu",
                 chat_id=update.effective_chat.id)
         #writer.write()
     writer.write()
@@ -137,11 +155,13 @@ def tgl_alle(update, context):
     if s["all"]:
         s["all"] = False
         context.bot.send_message(text="Sie haben sich aus alles umfassenden Updater ausgetragen, \
-es gelten wieder Ihre Einstellungen für einzelne Abos.",
+es gelten wieder Ihre Einstellungen für einzelne Abos.\n\
+            /abo   /zeig   /menu",
                 chat_id=update.effective_chat.id)
     else:
         s["all"] = True
         context.bot.send_message(text="Sie werden alle Updates erhalten, \
-Ihre aktuellen Abo Einstellungen werden ignoriert.",
+Ihre aktuellen Abo Einstellungen werden ignoriert.\n\
+            /abo   /zeig   /menu",
                 chat_id=update.effective_chat.id)
     writer.write()
