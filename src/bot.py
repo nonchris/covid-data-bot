6e56e9e466f9bcdb8a61a87ad820bd3456e8e429
nonchris <git@chris-ge.de>
2020-12-28 11:20:12 +0100

    :arrow_upper_right: Added import for error handling
    Importing telegram errors to handle  errors like 'Unauthorized chat', which made the bot crash

--- src/bot.py
+++ src/bot.py
@@ -7,6 +7,7 @@ from telegram.ext import Updater
 from telegram.ext import CommandHandler
 from telegram.ext import MessageHandler, Filters
 from telegram import Bot
+from telegram import error
 
 import bot_handlers as btc
 import toggle_subs as tgs
