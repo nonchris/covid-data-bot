6728e987167df98ff859ef5ee0eb09251438fdd1
nonchris <git@chris-ge.de>
2021-01-23 02:02:33 +0100

    :arrow_upper_right: Reformatted bot file
    Just make the code look a bit better - no new functions :)

--- src/bot.py
+++ src/bot.py
@@ -32,17 +32,14 @@ dispatcher = updater.dispatcher
 
 bot = Bot(token=API_Key)
 
-
 logging.basicConfig(
-        filename="data/events.log",
-        level= logging.INFO,
-        style="{",
-        format="[{asctime}] [{levelname}] [{name}] {message}")
+    filename="data/events.log",
+    level=logging.INFO,
+    style="{",
+    format="[{asctime}] [{levelname}] [{name}] {message}")
 
 
 def send_update(date):
-
-
     analyzer = ana.Analyzer(date)
 
     for city in utils.city_list:
@@ -51,16 +48,18 @@ def send_update(date):
         for chat in writer.entries:
             s = chat.settings
             if s[city.lower()] or s["all"]:
-                #print(path)
+                # print(path)
                 try:
                     logging.info(f"SENDING {city: <12} to {chat.id}")
                     bot.send_photo(chat.id, photo=open(path, 'rb'))
                 except error.Unauthorized:
                     logging.info(f"Blocked by {chat.username: <12} {chat.id} - passing")
 
-                time.sleep(0.04) #block that makes sure that 30 messages per second aren't exceeded
+                time.sleep(0.04)  # block that makes sure that 30 messages per second aren't exceeded
             else:
-                logging.info(f"Ignoring {city: <11} on {chat.id} - {s[city.lower()]} ({type(s[city.lower()])}) - {s['all']} ({type(s['all'])})")
+                logging.info(
+                    f"Ignoring {city: <11} on {chat.id} - {s[city.lower()]} ({type(s[city.lower()])}) - {s['all']} ({type(s['all'])})")
+
 
 def make_request():
     while True:
@@ -71,28 +70,26 @@ def make_request():
             logging.info("Sent all messages, sleeping until tomorrow")
 
             d = datetime.datetime.now()
-            till_tomorrow = ((24 - d.hour - 1) * 60 * 60)\
-            + ((60 - d.minute - 1) * 60)\
-            + (60 - d.second)\
-            + (3600 * 7) #earliest request starts at 7 am
+            till_tomorrow = ((24 - d.hour - 1) * 60 * 60) \
+                            + ((60 - d.minute - 1) * 60) \
+                            + (60 - d.second) \
+                            + (3600 * 7)  # earliest request starts at 7 am
             time.sleep(till_tomorrow)
 
         else:
             logging.info(f"No new data - sleeping for {round(REQUEST_INTERVAL / 60)} minutes")
-            time.sleep(REQUEST_INTERVAL) #requesting every hour
+            time.sleep(REQUEST_INTERVAL)  # requesting every hour
 
 
 reqest_thrd = threading.Thread(target=make_request)
 reqest_thrd.start()
 
-
 writer = csv_database.Writer()
-#passing writer object into other files
+# passing writer object into other files
 cmd.setup(writer)
 msh.setup(writer)
 sbc.setup(writer)
 mdc.setup(writer)
-
 
 start_handler = CommandHandler('start', cmd.start)
 dispatcher.add_handler(start_handler)
@@ -103,31 +100,23 @@ dispatcher.add_handler(echo_handler)
 caps_handler = CommandHandler('caps', cmd.caps)
 dispatcher.add_handler(caps_handler)
 
-
-menu_show_handler = CommandHandler(['zeig_graph', 'zeig', 'show', 'zg', 'sh', 's', 'z'], \
-                                   cmd.menu_show)
+menu_show_handler = CommandHandler(['zeig_graph', 'zeig', 'show', 'zg', 'sh', 's', 'z'], cmd.menu_show)
 dispatcher.add_handler(menu_show_handler)
 
-
-menu_abo_handler = CommandHandler(['abo', 'sub', 'abonnieren'], \
-                                  cmd.menu_abo)
+menu_abo_handler = CommandHandler(['abo', 'sub', 'abonnieren'], cmd.menu_abo)
 dispatcher.add_handler(menu_abo_handler)
 
-
-menu_menu_handler = CommandHandler(['menu', 'menue'], \
-                                   cmd.menu_menu)
+menu_menu_handler = CommandHandler(['menu', 'menue'], cmd.menu_menu)
 dispatcher.add_handler(menu_menu_handler)
 
-show_handler = CommandHandler(['Adenau', 'Altenahr', 'Ahrweiler', 'Breisig', 'Brohltal',\
-                    'Grafschaft', 'Neuenahr', 'Remagen', 'Sinzig', \
-                    'Bad_Breisig', 'Bad_Neuenahr', 'Bad_Neuenahr_Ahrweiler'\
-                    ], cmd.show)
+show_handler = CommandHandler(['Adenau', 'Altenahr', 'Ahrweiler', 'Breisig', 'Brohltal',
+                               'Grafschaft', 'Neuenahr', 'Remagen', 'Sinzig',
+                               'Bad_Breisig', 'Bad_Neuenahr', 'Bad_Neuenahr_Ahrweiler'
+                               ], cmd.show)
 dispatcher.add_handler(show_handler)
-
 
 hilfe_handler = CommandHandler(['hilfe', 'hilf', 'help', 'h', 'abo', 'a', 'mehr', 'more'], cmd.help)
 dispatcher.add_handler(hilfe_handler)
-
 
 about_handler = CommandHandler('about', cmd.about)
 dispatcher.add_handler(about_handler)
@@ -139,77 +128,67 @@ notify_handler = CommandHandler("notify", mdc.notify_all)
 dispatcher.add_handler(notify_handler)
 
 ####
-#kreis
-kreis_handler = CommandHandler(['abo_kreis', 'sub_kreis',\
-                    'abokreis', 'subkreis', 'akreis', 'skreis'], \
-                               sbc.tgl_kreis)
+# kreis
+kreis_handler = CommandHandler(['abo_kreis', 'sub_kreis',
+                                'abokreis', 'subkreis', 'akreis', 'skreis'], sbc.tgl_kreis)
 dispatcher.add_handler(kreis_handler)
 
-#adenau
-adenau_handler = CommandHandler(['abo_adenau', 'sub_adenau',\
-                    'aboadenau', 'subadenau', 'aadenau', 'sadenau'], \
-                                sbc.tgl_adenau)
+# adenau
+adenau_handler = CommandHandler(['abo_adenau', 'sub_adenau',
+                                 'aboadenau', 'subadenau', 'aadenau', 'sadenau'], sbc.tgl_adenau)
 dispatcher.add_handler(adenau_handler)
 
-#altenahr
-altenahr_handler = CommandHandler(['abo_altenahr', 'sub_altenahr',\
-                    'aboaltenahr', 'subaltenahr', 'aaltenahr', 'saltenahr'], \
-                                  sbc.tgl_altenahr)
+# altenahr
+altenahr_handler = CommandHandler(['abo_altenahr', 'sub_altenahr',
+                                   'aboaltenahr', 'subaltenahr', 'aaltenahr', 'saltenahr'], sbc.tgl_altenahr)
 dispatcher.add_handler(altenahr_handler)
 
-#breisig
-breisig_handler = CommandHandler(['abo_breisig', 'sub_breisig', 'abobreisig', 'subbreisig',\
-                    'abreisig', 'abo_bad_breisig', 'sub_bad_breisig', 'sbreisig'], \
-                                 sbc.tgl_breisig)
+# breisig
+breisig_handler = CommandHandler(['abo_breisig', 'sub_breisig', 'abobreisig', 'subbreisig',
+                                  'abreisig', 'abo_bad_breisig', 'sub_bad_breisig', 'sbreisig'], sbc.tgl_breisig)
 dispatcher.add_handler(breisig_handler)
 
-#brohltal
-brohltal_handler = CommandHandler(['abo_brohltal', 'sub_brohltal',\
-                     'abobrohltal', 'subbrohltal', 'abrohltal', 'sbrohltal'], \
-                                  sbc.tgl_brohltal)
+# brohltal
+brohltal_handler = CommandHandler(['abo_brohltal', 'sub_brohltal',
+                                   'abobrohltal', 'subbrohltal', 'abrohltal', 'sbrohltal'], sbc.tgl_brohltal)
 dispatcher.add_handler(brohltal_handler)
 
-#grafschaft
-grafschaft_handler = CommandHandler(['abo_grafschaft', 'sub_grafschaft',\
-                     'abografschaft', 'subgrafschaft', 'agrafschaft', 'sgrafschaft'], \
+# grafschaft
+grafschaft_handler = CommandHandler(['abo_grafschaft', 'sub_grafschaft',
+                                     'abografschaft', 'subgrafschaft', 'agrafschaft', 'sgrafschaft'],
                                     sbc.tgl_grafschaft)
 dispatcher.add_handler(grafschaft_handler)
 
-#neuenahr/ahrweiler
-neuenahr_handler = CommandHandler(['abo_neuenahr', 'sub_neuenahr',\
-                     'aboneuenahr', 'subneuenahr', 'aneuenahr','abo_bad_neuenahr', 'sneuenahr',\
-                     'abo_ahrweiler', 'sub_ahrweiler',\
-                     'aboahrweiler', 'subahrweiler', 'aahrweiler', 'sahrweiler'], \
-                                  sbc.tgl_neuenahr)
+# neuenahr/ahrweiler
+neuenahr_handler = CommandHandler(['abo_neuenahr', 'sub_neuenahr',
+                                   'aboneuenahr', 'subneuenahr', 'aneuenahr', 'abo_bad_neuenahr', 'sneuenahr',
+                                   'abo_ahrweiler', 'sub_ahrweiler',
+                                   'aboahrweiler', 'subahrweiler', 'aahrweiler', 'sahrweiler'], sbc.tgl_neuenahr)
 dispatcher.add_handler(neuenahr_handler)
 
-#remagen
-remagen_handler = CommandHandler(['abo_remagen', 'sub_remagen',\
-                    'aboremagen', 'subremagen', 'aremagen', 'sremagen'], \
-                                 sbc.tgl_remagen)
+# remagen
+remagen_handler = CommandHandler(['abo_remagen', 'sub_remagen',
+                                  'aboremagen', 'subremagen', 'aremagen', 'sremagen'], sbc.tgl_remagen)
 dispatcher.add_handler(remagen_handler)
 
-#sinzig
-sinzig_handler = CommandHandler(['abo_sinzig', 'sub_sinzig',\
-                    'abosinzig', 'subsinzig', 'asinzig', 'ssinzig'], \
-                                sbc.tgl_sinzig)
+# sinzig
+sinzig_handler = CommandHandler(['abo_sinzig', 'sub_sinzig',
+                                 'abosinzig', 'subsinzig', 'asinzig', 'ssinzig'], sbc.tgl_sinzig)
 dispatcher.add_handler(sinzig_handler)
 
-#kreis
-kreis_handler = CommandHandler(['abo_kreis', 'sub_kreis',\
-                     'abokreis', 'subkreis', 'akreis', 'skreis'], \
-                               sbc.tgl_kreis)
+# kreis
+kreis_handler = CommandHandler(['abo_kreis', 'sub_kreis',
+                                'abokreis', 'subkreis', 'akreis', 'skreis'], sbc.tgl_kreis)
 dispatcher.add_handler(kreis_handler)
 
-#alle
-alle_handler = CommandHandler(['abo_alle', 'sub_alle',\
-                     'aboalle', 'suballe', 'aalle', 'salle'], \
-                              sbc.tgl_alle)
+# alle
+alle_handler = CommandHandler(['abo_alle', 'sub_alle',
+                               'aboalle', 'suballe', 'aalle', 'salle'], sbc.tgl_alle)
 dispatcher.add_handler(alle_handler)
 
-#['abo_', 'sub_', 'abo', 'sub', 'a']
+# ['abo_', 'sub_', 'abo', 'sub', 'a']
 
-#bot.send_message(chat_id=402239048, text="Automated text")
+# bot.send_message(chat_id=402239048, text="Automated text")
 
 updater.start_polling()
 updater.idle()
